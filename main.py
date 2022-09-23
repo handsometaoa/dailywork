87ac8e89a3ef4740fdf1ac747aab7330cdffedcd
handsometaoa <1527153028@qq.com>
2022-09-24 01:17:57 +0800

    修改节日天数“

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def get_weather():
   return weather['weather'], math.floor(weather['temp'])
 
 def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+  delta = today - datetime.strptime(start_date, "%Y-%m-%d")+1
   return delta.days
 
 def get_birthday():
@@ -50,4 +50,3 @@ wm = WeChatMessage(client)
 wea, temperature = get_weather()
 data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
-print(res)
