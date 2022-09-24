efe64fba044dd4f8de22ace179eca05150f23718
handsometaoa <1527153028@qq.com>
2022-09-24 10:54:06 +0800

    qwe123

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
 user_id = os.environ["USER_ID"]
-user_id2 = os.environ["USER_ID2"]
+user_id2 = os.environ["USER2_ID"]
 
 template_id = os.environ["TEMPLATE_ID"]
 
